27ab592fca42d3b287115c622b3d321de19e3f20
suhwan <drrobot333@naver.com>
2024-02-14 18:58:55 +0900

    feat: Add destination addr, latency

--- job/Job.py
+++ job/Job.py
@@ -1,10 +1,26 @@
+import time
+
 class Job:
-    def __init__(self, input):
+    def __init__(self, input, destination):
         self.input = input
         self.output = None
+        self.destination = destination
+        self.start_time = time.time()
     
     def run(self):
         self.output = self.input
 
     def get_output(self):
         return self.output
+    
+    def is_destination(self, other_destination):
+        if self.destination == other_destination:
+            return True
+        else:
+            return False
+        
+    def get_start_time(self):
+        return self.start_time
+    
+    def calc_latency(self):
+        return time.time() - self.start_time
