eb92d76971e1bac3ce0ef6c2f0087eadb5342ade
suhwan <drrobot333@naver.com>
2024-02-14 18:09:11 +0900

    feat: Add Sender.py

--- program/Program.py
+++ program/Program.py
@@ -5,20 +5,29 @@ from threading import Thread
 
 class Program:
 
-    def __init__(self, config, topic_dispatcher):
+    def __init__(self, sub_config, pub_configs, topic_dispatcher):
 
         self.queue = Queue()
 
-        self.config = config
+        self.sub_config = sub_config
+        self.pub_configs = pub_configs
         self.topic_dispatcher = topic_dispatcher
 
         self.subscriber = None
-        self.publisher = None
-        self.subscriber = MQTTclient.Subscriber(config=self.config, queue=self.queue)
-        self.publisher = MQTTclient.Publisher(config=self.config)
+        self.publisher = []
 
-        processor_process = Thread(target=self.message_processor)
-        processor_process.start()
+        self.init_subscriber()
+        self.init_publisher()
+
+        processor_thread = Thread(target=self.message_processor)
+        processor_thread.start()
+    
+    def init_subscriber(self):
+        self.subscriber = MQTTclient.Subscriber(config=self.sub_config, queue=self.queue)
+
+    def init_publisher(self):
+        for config in self.pub_configs:
+            self.publisher.append(MQTTclient.Publisher(config=config))
 
     def message_processor(self):
         while True:
@@ -26,11 +35,8 @@ class Program:
             message = self.queue.get()  
             if message:
                 callback = self.topic_dispatcher.get(message.topic, self.handle_unknown_topic)
-                # print(message.payload)
-                # print(message.topic)
-                callback_process = Thread(target=callback, args=(message.topic, message.payload.decode('utf8'), self.publisher, ))
-                callback_process.start()
-                # callback(message.topic, message.payload.decode('utf8'), self.publisher)
+                callback_thread = Thread(target=callback, args=(message.topic, message.payload.decode('utf8'), self.publisher, ))
+                callback_thread.start()
 
     def handle_unknown_topic(self, topic, data, publisher):
         # 알 수 없는 토픽 처리 로직
---
+++ program/Sender.py
@@ -0,0 +1,69 @@
+import sys, os, json
+ 
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from program import Program
+from job import Job
+
+import argparse
+import pickle
+import time
+
+"""
+
+고쳐야할 것:
+  - ProgramExample - start
+  - config
+  - topic_dispathcer
+
+
+def callback_example(topic, data, publisher):
+    print(f"topic: {topic}")
+    print(f"data: {data}")
+
+--> 이런 식으로 (topic, data, publisher)를 무조건 받아야함.
+
+"""
+
+class Sender(Program):
+    def __init__(self, sub_config, pub_configs):
+        self.sub_config = sub_config
+        self.pub_configs = pub_configs
+
+        super().__init__(self.sub_config, self.pub_configs, self.topic_dispatcher)
+
+    def send_dummy_job(self, data, sleep_time, iterate_time):
+        for _ in range(iterate_time):
+            dummy_job = Job(data)
+            dummy_job_bytes = pickle.dumps(dummy_job)
+            self.publisher[0].publish("job/packet", dummy_job_bytes)
+
+            time.sleep(sleep_time)
+
+        
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-dst', '--destination', type=str, default="")
+    argparser.add_argument('-t', '--topic', type=str, default="job/packet")
+    argparser.add_argument('-g', '--sleep_gap', type=float, default="0.1")
+    argparser.add_argument('-s', '--size', type=int, default="100")
+    argparser.add_argument('-i', '--iterate', type=int, default="100")
+    args = argparser.parse_args()
+
+    sub_config = {
+            "ip": "127.0.0.1", 
+            "port": 1883,
+            "topics": [
+                (args.topic, 0),
+            ],
+        }
+    
+    pub_configs = [
+        {
+            "ip": args.destination, 
+            "port": 1883,
+        }
+    ]
+    
+    sender = Sender(sub_config=sub_config, pub_configs=pub_configs)
+    sender.send_dummy_job("0"*args.size, args.sleep_gap, args.iterate)
