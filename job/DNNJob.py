5e6d4d7616ba4cfe5ef1950cdfa61ab7494d2847
suhwan <drrobot333@naver.com>
2024-02-22 11:56:51 +0900

    fix: Fix DNNJob member var

--- job/DNNJob.py
+++ job/DNNJob.py
@@ -6,8 +6,8 @@ from torchvision.models import resnet18, ResNet18_Weights
 class DNNJob(Job):
     def __init__(self, data, source, destination, id, info):
         # input = torch.ones(data)
-        data = torch.ones((len(data), len(data)))
-        super().__init__(input, source, destination, id, info)
+        data = torch.ones((3, len(data), len(data)))
+        super().__init__(data, source, destination, id, info)
         self.model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
 
     def run(self):
