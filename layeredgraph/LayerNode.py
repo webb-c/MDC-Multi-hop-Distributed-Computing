171872a276dc27a18907bc4447715ac526166cac
suhwan <drrobot333@naver.com>
2024-03-19 20:59:25 +0900

    feat: Remove Self type

--- layeredgraph/LayerNode.py
+++ layeredgraph/LayerNode.py
@@ -1,5 +1,3 @@
-from typing import Self
-
 class LayerNode:
     def __init__(self, ip: str, layer: int):
         self._ip = ip
@@ -11,19 +9,19 @@ class LayerNode:
     def get_layer(self):
         return self._layer
     
-    def is_same_layer(self, target_layer_node: Self) -> bool:
+    def is_same_layer(self, target_layer_node) -> bool:
         if self._layer == target_layer_node.get_layer():
             return True
         else:
             return False
         
-    def is_same_node(self, target_layer_node: Self) -> bool:
+    def is_same_node(self, target_layer_node)  -> bool:
         if self._ip == target_layer_node.get_ip():
             return True
         else:
             return False
         
-    def is_same_layer_node(self, target_layer_node: Self) -> bool:
+    def is_same_layer_node(self, target_layer_node) -> bool:
         if self.is_same_layer(target_layer_node) and self.is_same_node(target_layer_node):
             return True
         else:
