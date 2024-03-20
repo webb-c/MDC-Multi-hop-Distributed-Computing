296ad664fd3035bef7108ccd9cec676798122b91
suhwan <drrobot333@naver.com>
2024-03-20 15:55:04 +0900

    log

--- program/MDC.py
+++ program/MDC.py
@@ -53,6 +53,8 @@ class MDC(Program):
     def handle_subtask_info(self, topic, data, publisher):
         subtask_info: SubtaskInfo = pickle.loads(data)
 
+        print(subtask_info)
+
         self.job_manager.add_subtask(subtask_info)
     
     def get_network_info(self, topic, data, publisher):
