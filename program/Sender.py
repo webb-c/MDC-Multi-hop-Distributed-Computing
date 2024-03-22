85148e853e8831a817bacf4597c8515dd85814d0
suhwan <drrobot333@naver.com>
2024-03-23 01:19:04 +0900

    l

--- program/Sender.py
+++ program/Sender.py
@@ -72,6 +72,7 @@ class Sender(MDC):
             destination_ip = subtask_info.get_destination().get_ip()
 
             print("destination_ip", destination_ip)
+            print()
 
             dnn_output_bytes = pickle.dumps(dnn_output)
                 
