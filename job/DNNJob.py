3e049e81be624a03ef150a9dd8e421119ff1864c
suhwan <drrobot333@naver.com>
2024-03-22 20:56:12 +0900

    fix: Fix not using lib

--- job/DNNJob.py
+++ job/DNNJob.py
@@ -1,7 +1,4 @@
-import time
 from job.Job import Job
-import torch
-from torchvision.models import resnet18, ResNet18_Weights
 
 class DNNJob(Job):
     def __init__(self, data, source, destination, terminal_destination, id):
