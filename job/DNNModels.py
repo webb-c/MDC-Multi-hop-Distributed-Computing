5795552d69a8d377ad252fbdf9ce842cada6d01e
suhwan <drrobot333@naver.com>
2024-03-26 11:43:22 +0900

    l

--- job/DNNModels.py
+++ job/DNNModels.py
@@ -45,6 +45,7 @@ class DNNModels:
         for index, subtask in enumerate(self._subtasks[job_name]):
             x : torch.Tensor = subtask(x)
             input_shape = tuple(x.shape)
+            print(input_shape)
 
             flops, _, _ = calculate_flops(model=subtask, input_shape=input_shape, output_as_string=False, output_precision=4, print_results=False)
 
