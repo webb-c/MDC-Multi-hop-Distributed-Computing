fa86ddfab3cf686e5bc28f7309c290f382a2e935
suhwan <drrobot333@naver.com>
2024-04-01 18:21:35 +0900

    feat: Add send_numpy_array, recv_numpy_array

--- program/Communicator.py
+++ program/Communicator.py
@@ -14,12 +14,29 @@ class Communicator(Exception):
         self.debug_mode = debug_mode
         self.queue = self.init_queue(self.queue_name)
 
-    def send_omnet_message(self, msg:str):
+    def send_numpy_array(self, array: np.ndarray):
+        # NumPy 배열을 직렬화
+        msg = pickle.dumps(array)
+        if self.debug_mode:
+            print("Sending array:", array)
+        # 직렬화된 데이터 전송
+        self.queue.send(msg)
+    
+    def receive_numpy_array(self) -> np.ndarray:
+        # 메시지 수신
+        msg, _ = self.queue.receive(self.buffer_size)
+        # 직렬화 해제하여 NumPy 배열로 변환
+        array = pickle.loads(msg)
+        if self.debug_mode:
+            print("Received array:", array)
+        return array
+
+    def send_message(self, msg:str):
         if self.debug_mode:
             print("sending msg:", msg)
         self.queue.send(msg.encode('utf-8'))
 
-    def get_omnet_message(self) -> str:
+    def get_message(self) -> str:
         message, _ = self.queue.receive(self.buffer_size)
         response_str = message.decode('utf-8')
         if self.debug_mode:
