196561ed0ed99ff9c31e5432a0aaae119e6c0421
suhwan <drrobot333@naver.com>
2024-04-01 16:42:56 +0900

    feat: Change file directory

--- communication/__init__.py
+++ communication/__init__.py
@@ -2,4 +2,3 @@ from communication.NetworkInfo import NetworkInfo
 from communication.NodeInfo import NodeInfo
 from communication.NodeLinkInfo import NodeLinkInfo
 from communication.RequestBacklog import RequestBacklog
-from communication.Communicator import Communicator
