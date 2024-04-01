d53265bcd2728e2e3c422798e632ab15673c8cab
suhwan <drrobot333@naver.com>
2024-04-01 16:36:28 +0900

    feat: Delete underscore

--- communication/Communicator.py
+++ communication/Communicator.py
@@ -9,28 +9,28 @@ class Communicator(Exception):
         Exception (_type_)
     """
     def __init__(self, queue_name:str, buffer_size:int, debug_mode:bool):
-        self._queue_name = queue_name
-        self._buffer_size = buffer_size
-        self._debug_mode = debug_mode
-        self._queue = self._init_queue(self._queue_name)
+        self.queue_name = queue_name
+        self.buffer_size = buffer_size
+        self.debug_mode = debug_mode
+        self.queue = self.init_queue(self.queue_name)
 
-    def _send_omnet_message(self, msg:str):
-        if self._debug_mode:
+    def send_omnet_message(self, msg:str):
+        if self.debug_mode:
             print("sending msg:", msg)
-        self._queue.send(msg.encode('utf-8'))
+        self.queue.send(msg.encode('utf-8'))
 
-    def _get_omnet_message(self) -> str:
-        message, _ = self._queue.receive(self._buffer_size)
+    def get_omnet_message(self) -> str:
+        message, _ = self.queue.receive(self.buffer_size)
         response_str = message.decode('utf-8')
-        if self._debug_mode:
+        if self.debug_mode:
             print("receive msg:", response_str)
         return response_str
 
-    def _close_queue(self):
-        self._queue.close()
-        posix_ipc.unlink_message_queue(self._queue_name)
+    def close_queue(self):
+        self.queue.close()
+        posix_ipc.unlink_message_queue(self.queue_name)
 
-    def _init_queue(self, queue_name:str) -> posix_ipc.MessageQueue:
+    def init_queue(self, queue_name:str) -> posix_ipc.MessageQueue:
         try:
             queue = posix_ipc.MessageQueue(queue_name, posix_ipc.O_CREAT)
             print(f"Queue {queue_name} successfully created.")
